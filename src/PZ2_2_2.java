ba5250bc7c12863880033077a1cc7a9ff065440b
i.badretdinov <ilnurb@mail.ru>
2022-05-24 08:38:33 +0500

    qwer

---
+++ src/PZ2_2_2.java
@@ -0,0 +1,13 @@
+public class PZ2_2_2 {
+    public static void main(String[] args) {
+        int varint=5;
+        boolean varboolean = true;
+        double vardouble =5.5;
+        long varlong =11111111L;
+        short varshort =-10;
+        byte varbyte=10;
+        float varfloat =3.14f;
+        char varchar = 'A';
+
+    }
+}
