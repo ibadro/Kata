32129aadf510062ed0192abf733a427b91bafb5d
i.badretdinov <ilnurb@mail.ru>
2022-05-25 07:58:17 +0500

    qwer

--- src/Java0/String/Pz232.java
+++ src/Java0/String/Pz232.java
@@ -1,10 +1,11 @@
 package Java0.String;
-
+import java.util.Scanner;
 public class Pz232 {
     public static void main(String[] args) {
-        String name = "ilnur";
-
-        System.out.println(name);
+        Scanner in = new Scanner(System.in);
+        System.out.println("Как вас зовут?");
+        String name = in.nextLine();
+        System.out.printf ("Здравствуйте, %s",name+ '!' );
+        in.close();
     }
-
-}
+    }
